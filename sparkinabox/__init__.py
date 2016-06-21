de797b282b622fd749e37233fd7457a4aad48b9e
zero323 <2a453a67@opayq.com>
2016-06-21 23:53:26 +0200

    Render only required tagets

--- sparkinabox/__init__.py
+++ sparkinabox/__init__.py
@@ -8,14 +8,20 @@ import sys
 from sparkinabox.release import __author__, __version__
 from sparkinabox.render import render_docker
 
+targets = {
+    "local": ["base", "client"],
+    "standalone": ["base", "client", "master", "worker"]
+}
+
 
 def main(args):
     if os.path.exists(args.output_dir):
         print("Output directory {0} already exists. Exiting.".format(args.output_dir))
         sys.exit(1)
 
-    for target in ["base", "client", "master", "worker"]:
+    for target in targets[args.profile]:
         basedir = os.path.join(args.output_dir, target)
         os.makedirs(basedir)
         with open(os.path.join(basedir, "Dockerfile"), "w") as fw:
             fw.write(render_docker(args, target))
+
